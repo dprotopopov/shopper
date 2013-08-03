5b9ea4b7d04d5f20605e45943c182e16450d8bf4
Dmitry Protopopov <dmitry.protopopov@gmail.com>
2013-08-04 03:04:26 +0400

    rollback

--- js/snippets.js
+++ js/snippets.js
@@ -62,9 +62,7 @@ function loadURL(url){
 // Обработка try-catch требуется для совместимости с IE
 function debugWrite(a,b) {
 	try {
-		if (typeof console != "undefined") {
-			console.log(a+":"+b);
-		}
+		console.log(a+":"+b);
 	} catch (e) {
 	}
 }
